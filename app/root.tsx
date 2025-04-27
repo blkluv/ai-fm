830659b5003544858ebaefc36018c9ca5bf395b5
Matěj Pešl <matejpesl1@gmail.com>
2025-04-27 22:55:15 +0200

    feat: init

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,11 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import {Links, Meta, Outlet, Scripts, ScrollRestoration,} from "@remix-run/react";
+import type {LinksFunction} from "@remix-run/node";
 
 import "./tailwind.css";
+import {HeroUIProvider} from "@heroui/react";
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {rel: "preconnect", href: "https://fonts.googleapis.com"},
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
@@ -22,24 +17,26 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
+    <head>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <Meta/>
+      <Links/>
+    </head>
+    <body>
+    <HeroUIProvider>
+      {children}
+      <ScrollRestoration/>
+      <Scripts/>
+    </HeroUIProvider>
+    </body>
     </html>
   );
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet/>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
+
+import {heroui} from "@heroui/react";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}", "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -18,5 +20,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 } satisfies Config;
